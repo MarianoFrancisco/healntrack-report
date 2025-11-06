e3557439e69107dfb50a7c68f0a49ef2ac366676
LuisCif-20 <n.rodas225@gmail.com>
2025-11-06 14:58:32 -0600

    fix: udpate swagger config

--- report-service/src/main/java/com/sa/healntrack/report_service/common/infrastructure/config/SwaggerConfig.java
+++ report-service/src/main/java/com/sa/healntrack/report_service/common/infrastructure/config/SwaggerConfig.java
@@ -1,19 +1,15 @@
 package com.sa.healntrack.report_service.common.infrastructure.config;
 
-import java.util.List;
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
-import io.swagger.v3.oas.models.servers.Server;
 import lombok.RequiredArgsConstructor;
 
 @Configuration
 @RequiredArgsConstructor
-public class OpenApiConfig {
+public class SwaggerConfig {
 
     @Bean
     public OpenAPI openAPI() {
