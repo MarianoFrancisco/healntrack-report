88835d63316d954144d875b6cf6bd425d76cef01
LuisCif-20 <n.rodas225@gmail.com>
2025-11-07 14:23:05 -0600

    fix: update use case

--- report-service/src/main/java/com/sa/healntrack/report_service/transaction/infrastructure/adapter/out/persistence/repository/TransactionEntityRepository.java
+++ report-service/src/main/java/com/sa/healntrack/report_service/transaction/infrastructure/adapter/out/persistence/repository/TransactionEntityRepository.java
@@ -12,28 +12,28 @@ import org.springframework.data.repository.query.Param;
 import com.sa.healntrack.report_service.transaction.domain.TransactionProfit;
 import com.sa.healntrack.report_service.transaction.infrastructure.adapter.out.persistence.entity.TransactionEntity;
 
-public interface TransactionEntityRepository extends JpaRepository<TransactionEntity, UUID>, JpaSpecificationExecutor<TransactionEntity> {
-    
+public interface TransactionEntityRepository
+        extends JpaRepository<TransactionEntity, UUID>, JpaSpecificationExecutor<TransactionEntity> {
+
     boolean existsByReferenceId(UUID referenceId);
 
     @Query("""
-        SELECT new com.sa.healntrack.report_service.transaction.domain.TransactionProfit(
-            t.referenceId,
-            new com.sa.healntrack.report_service.area.domain.Area(a.id, a.name, a.entityReference),
-            SUM(CASE WHEN t.type = 'INCOME' THEN t.amount ELSE -t.amount END),
-            t.occurredAt
-        )
-        FROM TransactionEntity t
-        JOIN t.area a
-        WHERE (:areaId IS NULL OR a.id = :areaId)
-          AND (:startDate IS NULL OR t.occurredAt >= :startDate)
-          AND (:endDate IS NULL OR t.occurredAt <= :endDate)
-        GROUP BY t.referenceId, t.occurredAt, a.id, a.name, a.entityReference
-    """)
+                SELECT new com.sa.healntrack.report_service.transaction.domain.TransactionProfit(
+                    t.referenceId,
+                    new com.sa.healntrack.report_service.area.domain.Area(a.id, a.name, a.entityReference),
+                    SUM(CASE WHEN t.type = 'INCOME' THEN t.amount ELSE -t.amount END),
+                    t.occurredAt
+                )
+                FROM TransactionEntity t
+                JOIN t.area a
+                WHERE (:areaId IS NULL OR a.id = CAST(:areaId AS uuid))
+                  AND (:startDate IS NULL OR t.occurredAt >= CAST(:startDate AS date))
+                  AND (:endDate IS NULL OR t.occurredAt <= CAST(:endDate AS date))
+                GROUP BY t.referenceId, t.occurredAt, a.id, a.name, a.entityReference
+            """)
     List<TransactionProfit> findProfitsFiltered(
-        @Param("areaId") UUID areaId,
-        @Param("startDate") LocalDate startDate,
-        @Param("endDate") LocalDate endDate
-    );
+            @Param("areaId") UUID areaId,
+            @Param("startDate") LocalDate startDate,
+            @Param("endDate") LocalDate endDate);
 
 }
