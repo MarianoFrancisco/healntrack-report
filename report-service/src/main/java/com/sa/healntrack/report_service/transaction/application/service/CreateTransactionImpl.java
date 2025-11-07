9a49afacf7bf9d66e51718d153c428feb0b5fa00
LuisCif-20 <n.rodas225@gmail.com>
2025-11-07 13:20:21 -0600

    fix: update use case

--- report-service/src/main/java/com/sa/healntrack/report_service/transaction/application/service/CreateTransactionImpl.java
+++ report-service/src/main/java/com/sa/healntrack/report_service/transaction/application/service/CreateTransactionImpl.java
@@ -5,12 +5,10 @@ import org.springframework.transaction.annotation.Transactional;
 
 import com.sa.healntrack.report_service.area.application.port.out.persistence.FindAreaByName;
 import com.sa.healntrack.report_service.area.domain.Area;
-import com.sa.healntrack.report_service.common.application.exception.EntityAlreadyExistsException;
 import com.sa.healntrack.report_service.common.application.exception.EntityNotFoundException;
 import com.sa.healntrack.report_service.transaction.application.mapper.TransactionMapper;
 import com.sa.healntrack.report_service.transaction.application.port.in.create_transaction.CreateTransaction;
 import com.sa.healntrack.report_service.transaction.application.port.in.create_transaction.CreateTransactionCommand;
-import com.sa.healntrack.report_service.transaction.application.port.out.persistence.ExistsTransactionByReferenceId;
 import com.sa.healntrack.report_service.transaction.application.port.out.persistence.SaveTransaction;
 import com.sa.healntrack.report_service.transaction.domain.Transaction;
 
@@ -25,16 +23,8 @@ public class CreateTransactionImpl implements CreateTransaction {
     private final FindAreaByName findAreaByName;
     private final SaveTransaction saveTransaction;
 
-    private final ExistsTransactionByReferenceId existsTransactionByReferenceId;
-
     @Override
     public void create(CreateTransactionCommand command) {
-        boolean existsByReferenceId = existsTransactionByReferenceId
-                .existsByReferenceId(command.referenceId());
-        if (existsByReferenceId) {
-            throw new EntityAlreadyExistsException(
-                    "Ya existe una transaccion con referencia: " + command.referenceId());
-        }
         Area area = findAreaByName.findByName(command.area())
                 .orElseThrow(() -> new EntityNotFoundException(
                             "No existe un area con nombre: " + command.area()));
